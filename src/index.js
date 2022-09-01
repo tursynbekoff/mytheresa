4edaca0203adb774690766b36b4ac2e96ebb47fc
tursynbekoff <talgat.tursynbekov@gmail.com>
2022-09-01 21:19:09 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./scss/App.scss";
+
+const el = document.getElementById("root");
+
+ReactDOM.render(<App />, el);

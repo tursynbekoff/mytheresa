4a2cffe26a397cd9934e329209a9979f70cad9f1
tursynbekoff <talgat.tursynbekov@gmail.com>
2022-09-06 21:42:19 +0200

    webp possibilities

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -23,24 +23,30 @@ class Menu extends Component {
     return (
       <div className="carousel">
         <div className="card">
-          <img data-testid="hero" src={data[active].image} alt="product" />
-            <Link className="link" to={`/${data[active].link}`} >
-              {data[active].id} {" "} <i className="arrow right"></i>
-            </Link>
+          <picture>
+            <source srcset={data[active].image.replace(".jpg", ".webp")} type="image/webp"></source>
+            <img data-testid="hero" className="hero" src={data[active].image} alt={data[active].link} />
+          </picture>
+          <Link className="link" to={`/${data[active].link}`} >
+            {data[active].id} {" "} <i className="arrow right"></i>
+          </Link>
         </div>
         <div className="carousel-smaller">
         
           {Object.keys(data).map((item, index) => (
             // eslint-disable-next-line
-            <img
-              data-testid={`thumbnail${index}`}
-              key={data[item].image}
-              src={data[item].image}
-              className={index === active ? "active" : ""}
-              alt={data[item].link}
-              onClick={this.handleIndexClick}
-              data-index={index}
-            />
+            <picture>
+              <source srcset={data[item].image.replace(".jpg", ".webp")} type="image/webp"></source>
+              <img
+                data-testid={`thumbnail${index}`}
+                key={data[item].image}
+                src={data[item].image}
+                className={`thumbnail ${index === active ? "active" : ""}`}
+                alt={data[item].link}
+                onClick={this.handleIndexClick}
+                data-index={index}
+              />
+            </picture>
           ))}
         </div>
       </div>
